0f499db2ad12377407ce541fc64839035d15f3b0
Jack Arthur Harrhy <me@jackharrhy.com>
2019-02-08 20:46:50 -0330

    Timestamp information

--- src/MatchDetails.js
+++ src/MatchDetails.js
@@ -1,20 +1,31 @@
 import React, {Component} from 'react';
 
+import moment from 'moment';
+
 class MatchDetails extends Component {
   render() {
     const {
       leftTeam,
       rightTeam,
+      when,
     } = this.props;
 
     return (
       <div className="matchDetails">
         <div>
-          <div>
-            <p>{rightTeam.name}</p>
+          <div className="matchDetails-names">
+            <div>
+              <p>{rightTeam.name}</p>
+            </div>
+            <div>
+              <p>{leftTeam.name}</p>
+            </div>
           </div>
-          <div>
-            <p>{leftTeam.name}</p>
+          <div className="matchDetails-timeStamp">
+            <div>
+              <h5>{moment(when).format('dddd, MMMM Do')}</h5>
+              <h4>{moment(when).format('LT')}</h4>
+            </div>
           </div>
         </div>
       </div>
