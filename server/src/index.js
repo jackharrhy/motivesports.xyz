6fca362c4dfb26eea608ca6bb1dd04e63a33009a
Jack Arthur Harrhy <me@jackharrhy.com>
2019-02-09 11:20:46 -0330

    Refactor server index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,34 +1,40 @@
 require('dotenv').config();
 
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-
 const cors = require('cors')
 const express = require('express');
 const path = require('path');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
-const app = express();
 const port = process.env.PORT || 4334;
 const clientDir = process.env.CLIENT_DIR || __dirname;
+
+const app = express();
 
 const adapter = new FileSync('data/db.json')
 const db = low(adapter);
 
 db.defaults({
-    teams: {},
-    matches: [],
+  teams: {},
+  matches: [],
 }).write();
 
-app.use(cors({credentials: true, origin: true}));
+app.use(cors({
+  credentials: true,
+  origin: true,
+}));
+
+const sendObject = (res, obj) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(obj));
+};
 
 app.get('/api/teams', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(db.get('teams').value()));
+  sendObject(res, db.get('teams').value());
 });
 
 app.get('/api/matches', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(db.get('matches').value()));
+  sendObject(res, db.get('matches').value());
 });
 
 app.use('/', express.static(path.resolve(clientDir)));
